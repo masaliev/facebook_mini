ce5ec3475de30abb518907d2a245e7b35df7d359
Berdimurat Masaliev <muratmbt@gmail.com>
2017-08-02 10:31:34 +0600

    Create tables if not exists

--- db/db.go
+++ db/db.go
@@ -17,6 +17,25 @@ func NewDataStorage(dbPath string) *DataStorage {
 	if err != nil{
 		panic(err)
 	}
+
+	//Init tables
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, full_name VARCHAR(255), password VARCHAR(500), picture VARCHAR(255), phone varchar(20));")
+	if err != nil{
+		panic(err)
+	}
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY, text TEXT, user_id INTEGER, comments_count INTEGER, like_count INTEGER, create_date DATETIME DEFAULT CURRENT_TIMESTAMP);")
+	if err != nil{
+		panic(err)
+	}
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS likes (id INTEGER PRIMARY KEY, user_id INTEGER, post_id INTEGER, create_date DATETIME DEFAULT CURRENT_TIMESTAMP);")
+	if err != nil{
+		panic(err)
+	}
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS comments (id INTEGER PRIMARY KEY, text TEXT, post_id INTEGER, user_id INTEGER, create_date DATETIME DEFAULT CURRENT_TIMESTAMP);")
+	if err != nil{
+		panic(err)
+	}
+
 	dataStorage.db = db
 	return dataStorage
 }
