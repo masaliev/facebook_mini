5713a4139eb4277c8375a75470e57e1886d7c11d
Berdimurat Masaliev <muratmbt@gmail.com>
2017-08-02 10:54:46 +0600

    Add flag bind_addr

--- main.go
+++ main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"github.com/masaliev/facebook_mini/api"
 	"os"
+	"flag"
 )
 
 func main() {
+
+	bindAddr := flag.String("bind_addr", ":8090", "Set bind address")
+	flag.Parse()
+
 	if _, err := os.Stat("public"); os.IsNotExist(err) {
 		os.MkdirAll("public/avatars", 0775)
 	}
-	a := api.NewApi(":8090", "./facebook_mini.db")
+	a := api.NewApi(*bindAddr, "./facebook_mini.db")
 	a.Start()
 	a.WaitStop()
 }
