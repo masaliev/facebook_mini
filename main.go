4efbad8b1517f4dff440c918c2c963c99ec02ba5
Berdimurat Masaliev <muratmbt@gmail.com>
2017-08-02 09:57:47 +0600

    Create avatars directory if not exist

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
-import "github.com/masaliev/facebook_mini/api"
+import (
+	"github.com/masaliev/facebook_mini/api"
+	"os"
+)
 
 func main() {
+	if _, err := os.Stat("public"); os.IsNotExist(err) {
+		os.MkdirAll("public/avatars", 0775)
+	}
 	a := api.NewApi(":8090", "./facebook_mini.db")
 	a.Start()
 	a.WaitStop()
